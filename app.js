a5459ee6e8307bfaef5ad82653aa9213f25576e7
wided <widedkhadhraoui2000@gmail.com>
2024-08-31 18:41:07 +0200

    5eme commit

--- app.js
+++ app.js
@@ -26,7 +26,6 @@ app.get('/', (req, res) => {
   res.render('login', { imagePath: 'public/images/lac.png' });
 });
 
-
 // Route pour gérer la connexion
 app.post('/login', async (req, res) => {
   const { email, password } = req.body;
@@ -48,28 +47,36 @@ app.post('/login', async (req, res) => {
 
 // Route pour gérer l'inscription
 app.post('/signup', async (req, res) => {
-  const { email, password } = req.body;
-  
   try {
-    const hashedPassword = await bcrypt.hash(password, 10);
-    await sql.query`INSERT INTO utilisateurs (email, password) VALUES (${email}, ${hashedPassword})`;
-    res.redirect('/');
+      const { prenom, nom, email, numero_telephone, date_naissance, password } = req.body;
+
+      // Hachage du mot de passe
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      // Insertion des données dans la base de données
+      await sql.query`
+          INSERT INTO utilisateurs (prenom, nom, email, numero_telephone, date_naissance, password)
+          VALUES (${prenom}, ${nom}, ${email}, ${numero_telephone}, ${date_naissance}, ${hashedPassword})
+      `;
+
+      // Redirection vers la page de connexion après l'inscription
+      res.redirect('/');
   } catch (err) {
-    res.status(500).send(err.message);
+      console.error('Erreur lors de l\'inscription :', err.message);
+      res.status(500).send('Erreur serveur');
   }
 });
-
 // Route pour le tableau de bord VR
 app.get('/vr', async (req, res) => {
   try {
     // Première requête pour obtenir le nombre de clients et le solde total
     const summaryResult = await sql.query(`
-      SELECT 
-          COUNT(name) AS clientCount,
-          SUM(cumulative_consumption_amount) AS totalBalance
-      FROM VR
-      WHERE CAST(last_consumption_time AS DATE) >= '2024-01-13';
-    `);
+    SELECT 
+        COUNT(name) AS clientCount,
+        SUM(cumulative_consumption_amount) AS totalBalance
+    FROM VR
+    WHERE CAST(last_consumption_time AS DATE) >= '2024-01-13';
+`);
 
     // Deuxième requête pour obtenir les détails des clients pour le tableau
     const clientsResult = await sql.query(`
@@ -94,22 +101,28 @@ app.get('/vr', async (req, res) => {
 
 
     // Troisième requête pour obtenir les statistiques pour le graphique en barres
-    const chartResult = await sql.query(`
-      SELECT
-        COUNT(*) AS numberOfClients,  -- Nombre de clients distincts
-        SUM(VR.cumulative_consumption_amount) AS solde,  -- Somme des montants de consommation cumulés
-        SUM(VR.Deposit_amount) AS remainingBalance,  -- Somme des montants de dépôt restants
-        COALESCE(SUM(export_client.clientNbVisite), 0) AS returningClients  -- Somme des visites de clients (avec 0 si pas de visites)
-      FROM VR
-      LEFT JOIN export_client ON VR.name = export_client.clientFirstname
-      WHERE CAST(last_consumption_time AS DATE) >= '2024-01-13';
-    `);
+   const chartResult = await sql.query(`
+  SELECT
+    COUNT( VR.name) AS numberOfClients,  -- Nombre de clients distincts
+    SUM(VR.cumulative_consumption_amount) AS solde,  -- Somme des montants de consommation cumulés
+    SUM(VR.Deposit_amount) AS remainingBalance,  -- Somme des montants de dépôt restants
+    COALESCE(SUM(sub.returningClients), 0) AS returningClients  -- Somme des visites de clients distinctes dans jeux1
+  FROM VR
+  LEFT JOIN export_client ON VR.name = export_client.clientLastname
+  LEFT JOIN (
+    SELECT name, COUNT(DISTINCT CAST(date AS DATE)) AS returningClients
+    FROM jeux1
+    GROUP BY name
+  ) sub ON VR.name = sub.name
+  WHERE CAST(last_consumption_time AS DATE) >= '2024-01-13';
+`);
+
 
 // Quatrième requête pour obtenir la somme totale des visites, des jeux et des soldes pour le graphique en doughnut
 const doughnutDataResult = await sql.query(`
   SELECT 
     COUNT(DISTINCT( jeux1.nomdejeux)) AS total_jeux,
-    SUM(jeux1.nbvisite) AS total_visites
+    COUNT(DISTINCT(name)) AS total_visites
 FROM jeux1
 WHERE CAST(jeux1.date AS DATE) >= '2024-01-13';
 `);
@@ -118,7 +131,7 @@ WHERE CAST(jeux1.date AS DATE) >= '2024-01-13';
 // Requête pour obtenir le nombre de meilleurs clients
 const topClientsResult = await sql.query(`
   SELECT
-      COUNT(name) AS top_clients
+      COUNT(DISTINCT(name)) AS top_clients
   FROM jeux1
   WHERE CAST(date AS DATE) >= '2024-01-13';
 `);
@@ -210,20 +223,20 @@ app.get('/top-clients', async (req, res) => {
   try {
       
       // Requête pour obtenir les données des meilleurs clients
-      const result = await sql.query(`
-       SELECT
-    jeux1.name AS nom,
-    COUNT(DISTINCT jeux1.nomdejeux) AS nombre_de_jeux,
-    SUM(jeux1.nbvisite) AS nombre_de_visites,
-    COALESCE(VR.cumulative_consumption_amount, 0) AS solde,
-    COALESCE(VR.Deposit_amount, 0) AS reste_du_solde
-FROM jeux1
-LEFT JOIN VR ON jeux1.name = VR.name
-WHERE CAST(jeux1.date AS DATE) >= '2024-01-13'
-GROUP BY jeux1.name, VR.cumulative_consumption_amount, VR.Deposit_amount
-ORDER BY nombre_de_visites DESC;
+  const result = await sql.query(`
+    SELECT
+        jeux1.name AS nom,
+        COUNT(DISTINCT jeux1.nomdejeux) AS nombre_de_jeux,
+        COUNT(DISTINCT CAST(jeux1.date AS DATE)) AS nombre_de_visites,
+        COALESCE(VR.cumulative_consumption_amount, 0) AS solde,
+        COALESCE(VR.Deposit_amount, 0) AS reste_du_solde
+    FROM jeux1
+    LEFT JOIN VR ON jeux1.name = VR.name
+    WHERE CAST(jeux1.date AS DATE) >= '2024-01-13'
+    GROUP BY jeux1.name, VR.cumulative_consumption_amount, VR.Deposit_amount
+    ORDER BY nombre_de_visites DESC;
+`);
 
-    `);
 
 
       // Rendre la vue avec les données
@@ -251,16 +264,123 @@ app.get('/fid', (req, res) => {
     });
 });
 
-// Route pour le tableau de bord PME
-app.get('/pme', (req, res) => {
-    res.render('pme', {
-        title: 'PME',
-        visitCount: 100,           // Valeur d'exemple
-        withdrawalCount: 100,      // Valeur d'exemple
-        activityCount: 5,          // Valeur d'exemple
-        clientCount: 5             // Valeur d'exemple
-    });
+app.get('/profile', async (req, res) => {
+  try {
+      // Vérification si l'utilisateur est connecté
+      if (!req.session.user) {
+          return res.redirect('/login'); // Redirige vers la page de connexion si non connecté
+      }
+
+      // Récupération de l'email de l'utilisateur connecté à partir de la session
+      const email = req.session.user.email;
+
+      // Récupération des informations de l'administrateur à partir de la base de données
+      const request = new sql.Request();
+      request.input('email', sql.VarChar, email);
+
+      const result = await request.query(`
+          SELECT prenom, nom, email, numero_telephone, date_naissance
+          FROM [crm].[wided].[utilisateurs]
+          WHERE email = @email
+      `);
+
+      if (result.recordset.length === 0) {
+          return res.status(404).send('Administrateur non trouvé');
+      }
+
+      const admin = result.recordset[0];
+
+      // Formatage de la date de naissance
+      const dateNaissance = new Date(admin.date_naissance);
+      admin.jour_naissance = dateNaissance.getDate();
+      admin.mois_naissance = dateNaissance.getMonth() + 1; // Les mois sont indexés à partir de 0
+      admin.annee_naissance = dateNaissance.getFullYear();
+
+      // Rendu de la vue 'profile' avec les informations de l'administrateur
+      res.render('profile', { 
+          title: 'Profil Administrateur',
+          admin: admin 
+      });
+  } catch (err) {
+      console.error('Erreur lors de la récupération du profil :', err.message);
+      res.status(500).send('Erreur serveur');
+  }
 });
+
+// Afficher le formulaire de modification du profil
+app.get('/profile/edit', async (req, res) => {
+  try {
+      if (!req.session.user) {
+          return res.redirect('/login');
+      }
+
+      const email = req.session.user.email;
+
+      const request = new sql.Request();
+      request.input('email', sql.VarChar, email);
+
+      const result = await request.query(`
+          SELECT prenom, nom, email, numero_telephone, date_naissance
+          FROM [crm].[wided].[utilisateurs]
+          WHERE email = @email
+      `);
+
+      if (result.recordset.length === 0) {
+          return res.status(404).send('Administrateur non trouvé');
+      }
+
+      const admin = result.recordset[0];
+      const dateNaissance = new Date(admin.date_naissance);
+      admin.jour_naissance = dateNaissance.getDate();
+      admin.mois_naissance = dateNaissance.getMonth() + 1;
+      admin.annee_naissance = dateNaissance.getFullYear();
+
+      res.render('profile-edit', {
+          title: 'Modifier le Profil',
+          admin: admin
+      });
+  } catch (err) {
+      console.error('Erreur lors de l\'affichage du formulaire de modification :', err.message);
+      res.status(500).send('Erreur serveur');
+  }
+});
+
+// Traitement de la modification du profil
+app.post('/profile/edit', async (req, res) => {
+  try {
+      if (!req.session.user) {
+          return res.redirect('/login');
+      }
+
+      const email = req.session.user.email;
+      const { prenom, nom, numero_telephone, date_naissance } = req.body;
+
+      const request = new sql.Request();
+      request.input('prenom', sql.VarChar, prenom);
+      request.input('nom', sql.VarChar, nom);
+      request.input('numero_telephone', sql.VarChar, numero_telephone);
+      request.input('date_naissance', sql.Date, new Date(date_naissance));
+      request.input('email', sql.VarChar, email);
+
+      await request.query(`
+          UPDATE [crm].[wided].[utilisateurs]
+          SET prenom = @prenom,
+              nom = @nom,
+              numero_telephone = @numero_telephone,
+              date_naissance = @date_naissance
+          WHERE email = @email
+      `);
+
+      res.redirect('/profile');
+  } catch (err) {
+      console.error('Erreur lors de la mise à jour du profil :', err.message);
+      res.status(500).send('Erreur serveur');
+  }
+});
+
+
+
+
 
 // Route pour la déconnexion
 app.get('/logout', (req, res) => {
